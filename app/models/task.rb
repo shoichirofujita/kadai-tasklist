092be567de9fd1640106021cd31004332e464fae
Shoichiro.Fujita <shichrfjt.0614@gmail.com>
2017-04-25 04:01:35 +0000

    post

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
 	validates :content, presence: true, length: { maximum: 255 }
 	validates :Status, presence: true, length: { maximum: 10 }
-	end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
   
-  get 'login', to: 'session#new'
-  post 'login', to: 'session#create'
-  delete 'login', to: 'session#destroy'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
-  get 'singup', to: 'users#new'
+  get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
 
-  
-  
   resources :tasks
   
+  resources :microposts, only: [:create, :destroy]
 end
